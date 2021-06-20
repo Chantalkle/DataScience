ec169c5212d95a29365eb4b93790fcee7760daf3
Chantalkle <33465110+Chantalkle@users.noreply.github.com>
2021-06-20 15:14:49 +0200

    Update kmeans_kmedian_webapp_final.py

--- kmeans_kmedian_webapp_final.py
+++ kmeans_kmedian_webapp_final.py
@@ -48,16 +48,15 @@ def get_data(data, settype):
     if settype == 'Wholesale customers':
         Y = []
         for entry in X:
-              Y.append(entry[2:])
-            
+              Y.append(entry[2:])  
         X=Y
         
     if settype == 'Wine':
         Y = [] # Attributes
-        
         for entry in X:
             Y.append(entry[1:]) 
         X = Y
+        
     min_max_scaler = preprocessing.MinMaxScaler()
     X_scaled= min_max_scaler.fit_transform(X)
     X_scaled = X_scaled.tolist()
